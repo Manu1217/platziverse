16e1e1a4405896a6970da9cb63828f65084b638e
Manu1217 <manux1217@gmail.com>
2018-03-07 11:16:50 -0600

    commit with fixes of agent and runnable examples, in mqtt server saves undefiend metrics TODO

--- platziverse-agent/examples/index.js
+++ platziverse-agent/examples/index.js
@@ -1,42 +1,40 @@
 const PlatziverseAgent = require('../')
 
 const agent = new PlatziverseAgent({
-    name: 'myapp',
-    username: 'admin',
-    interval: 2000 // que cada 2 seg muestre info
+  name: 'myapp',
+  username: 'admin',
+  interval: 2000  // que cada 2 seg muestre info
 })
 
-// resident set size
-agent.addMetric('rss', function getRss() {
-    return process.memoryUsage().rss
+agent.addMetric('rss', function getRss () {
+  return process.memoryUsage().rss
 })
 
-agent.addMetric('promiseMetric', function getRandomPromise(){
-    return Promise.resolve(Math.random())
+agent.addMetric('promiseMetric', function getRandomPromise () {
+  return Promise.resolve(Math.random())
 })
 
-agent.addMetric('callbackMetric', function getRandomCallback (callback){
-    setTimeout(() => {
-        calllback(null, Math.random())
-    }, 1000)
+agent.addMetric('callbackMetric', function getRandomCallback (callback) {
+  setTimeout(() => {
+    callback(null, Math.random())
+  }, 1000)
 })
+
 agent.connect()
 
-// this agent only
+// This agent only
 agent.on('connected', handler)
 agent.on('disconnected', handler)
 agent.on('message', handler)
 
-// other agents
-agent.on('agent/message', payload => {
-    console.log(payload)
-})
+// Other Agents
 agent.on('agent/connected', handler)
 agent.on('agent/disconnected', handler)
 agent.on('agent/message', handler)
 
 function handler (payload) {
-    console.log(payload)
+  console.log(payload)
 }
 
 setTimeout(() => agent.disconnect(), 10000)
+
--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,118 +1,121 @@
 'use strict'
 
 const debug = require('debug')('platziverse:agent')
-const mqtt = require('mqtt')
 const os = require('os')
 const util = require('util')
+const mqtt = require('mqtt')
 const defaults = require('defaults')
-const EventEmitter = require('events')
-const { parsePayload } = require('./utils')
 const uuid = require('uuid')
+const EventEmitter = require('events')
+
+const { parsePayload } = require('./utils')
 
 const options = {
-    name: 'untitled',
-    username: 'platzi',
-    interval: '5000',
-    mqtt: {
-        host: 'mqtt://localhost'
-    }
+  name: 'untitled',
+  username: 'platzi',
+  interval: 5000,
+  mqtt: {
+    host: 'mqtt://localhost'
+  }
 }
 
 class PlatziverseAgent extends EventEmitter {
-    constructor(opts) {
-        super() // llamamos al constructor de la clase extendida
-        this._options = defaults(opts, options)
-        this._starterd = false
-        this._timer = null
-        this._client = null
-        this._agentId = null
-        this._metrics = new Map()
-    }
+  constructor (opts) {
+    super()  // llamamos al constructor de la clase extendida
 
-    addMetric(type, fn) {
-        this._metrics.set(type, fn)
-    }
-    removeMetric(type) {
-        this._metrics.delete(type)
-    }
-    connect() {
-        if (!this._starterd) {
-            const opts = this._options
-            this._client = mqtt.connect(opts.mqtt.host)
-            this._starterd = true
+    this._options = defaults(opts, options)
+    this._started = false
+    this._timer = null
+    this._client = null
+    this._agentId = null
+    this._metrics = new Map()
+  }
 
-            /* Subscribo al cliente a los mensajes que vamos a recibir*/
+  addMetric (type, fn) {
+    this._metrics.set(type, fn)
+  }
 
-            this._client.subscribe('agent/message')
-            this._client.subscribe('agent/connected')
-            this._client.subscribe('agent/disconnected')
+  removeMetric (type) {
+    this._metrics.delete(type)
+  }
 
-            this._client.on('connect', () => {
-                this._agentId = uuid.v4()
+  connect () {
+    if (!this._started) {
+      const opts = this._options
+      this._client = mqtt.connect(opts.mqtt.host)
+      this._started = true
 
-                this.emit('connected')
-                this._timer = setInterval(async () => {
-                    if (this._metrics.size > 0) {
-                        let message = {
-                            agent: {
-                                uuid: this._agentId,
-                                username: opts.username,
-                                name: opts.name,
-                                hostname: os.hostname() || 'localhost',
-                                pid: process.pid
-                            },
-                            metrics: [],
-                            timestamp: new Date().getTime()
-                        }
-                    }
-                    
-                    for(let [ metric, fn ] of this._metrics){
-                        if (fn.length == 1) {
-                            fn = util.promisify(fn)
-                        }
+      this._client.subscribe('agent/message')
+      this._client.subscribe('agent/connected')
+      this._client.subscribe('agent/disconnected')
 
-                        message.metrics.push({
-                            type: metric,
-                            value: await Promise.resolve(fn())
-                        })
-                    }
-                    debug('Sending', message)
+      this._client.on('connect', () => {
+        this._agentId = uuid.v4()
 
-                    this._client.publish('agent/message', JSON.stringify(message))
-                    this.emit('message', message)
-                }, opts.interval)
-            })
+        this.emit('connected', this._agentId)
 
-            this._client.on('message', (topic, payload) => {
-                payload = parsePayload(payload)
+        this._timer = setInterval(async () => {
+          if (this._metrics.size > 0) {
+            let message = {
+              agent: {
+                uuid: this._agentId,
+                username: opts.username,
+                name: opts.name,
+                hostname: os.hostname() || 'localhost',
+                pid: process.pid
+              },
+              metrics: [],
+              timestamp: new Date().getTime()
+            }
 
-                let broadcast = false
+            for (let [ metric, fn ] of this._metrics) {
+              if (fn.length === 1) {
+                fn = util.promisify(fn)
+              }
 
-                switch (topic) {
-                    case 'agent/connected':
-                    case 'agent/disconnected':
-                    case 'agent/message':
-                        broadcast = payload && payload.agent && payload.agent.uuid != this._agentId
-                }
+              message.metrics.push({
+                type: metric,
+                value: await Promise.resolve(fn())
+              })
+            }
 
-                if (broadcast) {
-                    this.emit(topic, payload)
-                }
-            })
+            debug('Sending', message)
 
-            this._client.on('error', () => this.disconnect())
+            this._client.publish('agent/message', JSON.stringify(message))
+            this.emit('message', message)
+          }
+        }, opts.interval)
+      })
 
+      this._client.on('message', (topic, payload) => {
+        payload = parsePayload(payload)
+
+        let broadcast = false
+        switch (topic) {
+          case 'agent/connected':
+          case 'agent/disconnected':
+          case 'agent/message':
+            broadcast = payload && payload.agent && payload.agent.uuid !== this._agentId
+            break
         }
-    }
-    disconnect() {
-        if (this._starterd)  {
-            clearInterval(this._timer)
-            this._starterd = false
-            this.emit('disconnected', this._agentId)
-            this._client.end()
-        }
-    }
 
+        if (broadcast) {
+          this.emit(topic, payload)
+        }
+      })
+
+      this._client.on('error', () => this.disconnect())
+    }
+  }
+
+  disconnect () {
+    if (this._started) {
+      clearInterval(this._timer)
+      this._started = false
+      this.emit('disconnected', this._agentId)
+      this._client.end()
+    }
+  }
 }
 
 module.exports = PlatziverseAgent
